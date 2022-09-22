c2224169621f1085c45f257c945efe26bc2a2684
aaaicu <aaaaicu@gmail.com>
2022-09-23 00:50:51 +0900

    first commit

---
+++ src/main/java/kr/co/ojy/OjyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ojy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/ojy/OjyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ojy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
