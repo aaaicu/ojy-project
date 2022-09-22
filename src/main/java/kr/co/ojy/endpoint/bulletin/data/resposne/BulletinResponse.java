f0831201ad505a920b2ff96b10f91cb6748bf58c
aaaicu <aaaaicu@gmail.com>
2022-09-23 02:25:04 +0900

    feat: 기본 환경 세팅 - 컨트롤러, 스웨거

---
+++ src/main/java/kr/co/ojy/endpoint/bulletin/data/resposne/BulletinResponse.java
@@ -0,0 +1,12 @@
+package kr.co.ojy.endpoint.bulletin.data.resposne;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BulletinResponse {
+    private Long worshipActionId;
+}
